482fa162e4277e116856f76fc4077b8796997cfc
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-11-20 19:00:34 +0000

    Updated Create User Component:
    - Added form.

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
+import Button from '@atlaskit/button';
 
 export default class CreateUser extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: ''
+        };
+    };
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    };
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username
+        };
+
+        console.log(user);
+
+        this.setState({
+            username: ''
+        });
+    };
+
     render() {
         return (
             <div>
-                <p>You are on the Create User Component!</p>
+                {/* <p>You are on the Create User Component!</p> */}
+
+                <h3>Create New User</h3>
+
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Username: </label>
+
+                        <input 
+                            type="text"
+                            required
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                        />
+                    </div>
+
+                    <div>
+                        <Button
+                            appearance="primary"
+                            type="submit"
+                            value="Create User"
+                        >
+                            Submit
+                        </Button>
+                    </div>
+                </form>
             </div>
         );
     };
