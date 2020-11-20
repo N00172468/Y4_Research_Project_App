e13e56c6e1c8ba01228d8c9420e79631650b1262
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-11-20 19:07:32 +0000

    Connecting Frontend to Backend via Axios:
    - Conneted Create User component to backend.

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 import Button from '@atlaskit/button';
 
 export default class CreateUser extends Component {
@@ -27,6 +29,9 @@ export default class CreateUser extends Component {
         };
 
         console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
 
         this.setState({
             username: ''
