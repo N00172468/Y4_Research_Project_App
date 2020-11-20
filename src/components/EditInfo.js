22274f30df19efae42f68b65c049f7c29b8aca26
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-11-20 20:16:36 +0000

    Updated List and Update Component:
    - Made list component to display data from database.
    - Added connection to db in update component.
    - Debugged a very obvious typo that took too long to find.

--- src/components/EditInfo.js
+++ src/components/EditInfo.js
@@ -1,10 +1,174 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
+import Calendar from '@atlaskit/calendar';
+import Button from '@atlaskit/button';
+// import { response } from 'express';
 
 export default class EditInfo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeExperience = this.onChangeExperience.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+            description: '',
+            experience: 0,
+            date: new Date(),
+
+            users: []
+        };
+    };
+
+    componentDidMount() {
+        // this.setState({
+        //     users: ['test user'],
+        //     username: 'test user'
+        // });
+        axios.get('http://localhost:5000/info/' + this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    username: response.data.username,
+                    description: response.data.description,
+                    experience: response.data.experience,
+                    date: new Date(response.data.date)
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username)
+                    });
+                }
+            });
+    };
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    };
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    };
+
+    onChangeExperience(e) {
+        this.setState({
+            experience: e.target.value
+        });
+    };
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    };
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const info = {
+            username: this.state.username,
+            description: this.state.description,
+            experience: this.state.experience,
+            date: this.state.date
+        };
+
+        console.log(info);
+
+        axios.post('http://localhost:5000/info/update/' + this.props.match.params.id, info)
+            .then(res => console.log(res.data));
+
+        window.location = '/';
+    };
+
     render() {
         return (
             <div>
-                <p>You are on the Edit Info Component!</p>
+                {/* <p>You are on the Create Info Component!</p> */}
+
+                <h3>Update Info Log</h3>
+
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Username: </label>
+
+                        <select 
+                            ref="userInput"
+                            required
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                        >
+                            {
+                                this.state.users.map(function(user) {
+                                    return <option
+                                                key={user}
+                                                value={user}
+                                            >
+                                            	{user}
+                                            </option>
+                                })
+                            };
+                        </select>
+                    </div>
+
+                    <div>
+                        <label>Description: </label>
+
+                        <input
+                            type="text"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                        />
+                    </div>
+
+                    <div>
+                        <label>Experience (in Years): </label>
+
+                        <input
+                            type="number"
+                            value={this.state.experience}
+                            onChange={this.onChangeExperience}
+                        />
+                    </div>
+
+                    <div>
+                        <label>Date: </label>
+
+                        <Calendar
+                            innerProps={{
+                                style: {
+                                    border: '1px solid blue',
+                                    display: 'inline-block'
+                                }
+                            }}
+                            onChange={this.onChangeDate}
+                        />
+                    </div>
+
+                    <div>
+                        <Button
+                            appearance="primary"
+                            type="submit"
+                            value="Update Info Log"
+                        >
+                            Submit
+                        </Button>
+                    </div>
+                </form>
             </div>
         );
     };
--- src/components/InfoList.js
+++ src/components/InfoList.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+const Info = props => (
+    <tr>
+        <td>{props.info.username}</td>
+        <td>{props.info.description}</td>
+        <td>{props.info.experience}</td>
+        <td>{props.info.date.substring(0,10)}</td>
+        <td>
+            <Link to={"/edit/" + props.info._id}>Edit</Link> | <a href="#" onClick={() => { props.deleteInfo(props.info._id) }}>Delete</a>
+        </td>
+    </tr>
+);
 
 export default class InfoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.deleteInfo = this.deleteInfo.bind(this);
+
+        this.state = {
+            info: []
+        };
+    };
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/info/')
+            .then(response => {
+                this.setState({ info: response.data })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    };
+
+    deleteInfo(id) {
+        axios.delete('http://localhost:5000/info/' + id)
+            .then(res => console.log(res.data)
+        );
+        
+        this.setState({
+            info: this.state.info.filter(el => el._id !== id)
+        });
+    };
+
+    infoList() {
+        return this.state.info.map(currentInfo => {
+            return <Info info={currentInfo} deleteInfo={this.deleteInfo} key={currentInfo._id}/>;
+        })
+    };
+
     render() {
         return (
             <div>
-                <p>You are on the Info List Component!</p>
+                <h3>Logged Info</h3>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Experience</th>
+                            <th>Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        { this.infoList() }
+                    </tbody>
+                </table>
             </div>
         );
     };
